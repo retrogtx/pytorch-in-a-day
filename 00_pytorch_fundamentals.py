8c1540a19364822c9cd759c533e6fd79c715e47e
Amrit Rai <iamamrit27@gmail.com>
2024-04-14 15:09:56 +0530

    Add PyTorch fundamentals code with random tensors and zero/ones tensors

--- 00_pytorch_fundamentals.py
+++ 00_pytorch_fundamentals.py
@@ -30,6 +30,32 @@ print(matrix.ndim)
 print(matrix.shape)
 print(matrix[0])
 
-Tensor = torch.tensor([[[1, 2, 3], [4, 5, 6], [8, 4, 5]]])
+Tensor = torch.tensor(
+    [[[1, 2, 3], [4, 5, 6], [8, 4, 5]]]
+)  # Tensor is a multi-dimensional array of numbers
 print(Tensor.ndim)
 print(Tensor.shape)
+
+"""
+Random Tensors:
+Why random tensors?
+
+Random tensors are important because the way many neural networks learn is that they start with tensors full of random numbers and then
+they adjust those numbers until they represent the patterns in the data that they're trying to learn.
+
+start with random numbers -> look at data -> update random numbers -> look at data -> update random numbers
+"""
+
+random_tensor = torch.rand(3, 3)
+print(random_tensor)
+
+# Random tensor with similar shape to an image vector
+image = torch.rand(size=(224, 224, 3))  # height, width, colour channels
+print(image.shape, image.ndim)
+
+# Zero and Ones Tensors
+zero_tensor = torch.zeros(3, 3)
+print(zero_tensor * random_tensor)
+
+ones_tensor = torch.ones(3, 3)
+print(ones_tensor * random_tensor)
